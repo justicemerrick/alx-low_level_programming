16ad66f2132e6cf4035b3dd2091ecf9b6ad28f7d
justicemerrick <justicemer@gmail.com>
2022-07-01 16:41:21 -0700

    error correction

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,22 +6,28 @@
  */
 int main(void)
 {
-	int ch;
-	int n;
-	for (ch = 48; ch<= 57; ch++)
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (n = 48; n <= 57; n++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			putchar(ch);
-			putchar(n);
-			if (ch != 57 || n != 57)
-			{
-				putchar(44);
-				putchar(32);
-			}
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+
+			if (num1 == 98 && num2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
 		}
 	}
-	putchar(10);
+
+	putchar('\n');
 
 	return (0);
+
 }
