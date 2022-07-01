0bb9118ccf669be034de3f4cddb79aaf7a0696a6
justicemerrick <justicemer@gmail.com>
2022-07-01 13:14:03 -0700

    another correction

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,16 +14,16 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-printf("Last digit of %d is ", n);
+printf("Last digit of %d is ",n);
 if (n > 5)
 {
 	printf("greater than 5");
 }
-else if (n == 0)
+if ( n == 0)
 {
 	printf("0");
 }
-else (n < 0 && n != 0)
+if (n < 0 && n != 0)
 {
 	printf("less than 6 not 0");
 }
