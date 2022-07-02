4411ad1c1e70eda89e7d9f4c0d92d55bfb9c3480
justicemerrick <justicemer@gmail.com>
2022-07-01 17:24:30 -0700

    6-sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	float d;
 	char c;
 
-printf("size of a char: %d byte(s)\n", sizeof(c));
-printf("size of an int: %d byte(s)\n", sizeof(i));
-printf("size of a long int: %d byte(s)\n", sizeof(l));
-printf("size of a long long int: %d byte(s)\n", sizeof(ll));
-printf("size of a float: %d byte(s)\n", sizeof(d));
+printf("size of a char: %lu byte(s)\n", sizeof(c));
+printf("size of an int: %lu byte(s)\n", sizeof(i));
+printf("size of a long int: %lu byte(s)\n", sizeof(l));
+printf("size of a long long int: %lu byte(s)\n", sizeof(ll));
+printf("size of a float: %lu byte(s)\n", sizeof(d));
 
 return (0);
 }
