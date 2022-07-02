81716c78dd62305ceebd164ec901ae2e63bf4b8b
justicemerrick <justicemer@gmail.com>
2022-07-01 17:18:52 -0700

    sizes commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ printf("size of a char: %d byte(s)\n", sizeof(c));
 printf("size of an int: %d byte(s)\n", sizeof(i));
 printf("size of a long int: %d byte(s)\n", sizeof(l));
 printf("size of a long long int: %d byte(s)\n", sizeof(ll));
-printf("size of float: %d byte(s)\n", sizeof(d));
+printf("size of a float: %d byte(s)\n", sizeof(d));
 
 return (0);
 }
