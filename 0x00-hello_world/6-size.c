dc3c2be046c5b08982b5b03ca4d1500125d8af3f
justicemerrick <justicemer@gmail.com>
2022-07-03 07:47:37 -0700

    size correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	float d;
 	char c;
 
-printf("size of a char: %lu byte(s)\n", sizeof(c));
-printf("size of an int: %lu byte(s)\n", sizeof(i));
-printf("size of a long int: %lu byte(s)\n", sizeof(l));
-printf("size of a long long int: %lu byte(s)\n", sizeof(ll));
-printf("size of a float: %lu byte(s)\n", sizeof(d));
+printf("size of a char: %f byte(s)\n", sizeof(c));
+printf("size of an int: %f byte(s)\n", sizeof(i));
+printf("size of a long int: %f byte(s)\n", sizeof(l));
+printf("size of a long long int: %f byte(s)\n", sizeof(ll));
+printf("size of a float: %f byte(s)\n", sizeof(d));
 
 return (0);
 }
