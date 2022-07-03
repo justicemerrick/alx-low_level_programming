2026fc7a3de1fc237a93ca0fe0e9d8aeead4581f
justicemerrick <justicemer@gmail.com>
2022-07-03 08:02:17 -0700

    error correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,10 @@ int main(void)
 	float d;
 	char c;
 
+printf("size of an int: %d byte(s)\n", sizeof(i));
 printf("size of a char: %f byte(s)\n", sizeof(c));
-printf("size of an int: %f byte(s)\n", sizeof(i));
 printf("size of a long int: %f byte(s)\n", sizeof(l));
 printf("size of a long long int: %f byte(s)\n", sizeof(ll));
 printf("size of a float: %f byte(s)\n", sizeof(d));
-
 return (0);
 }
