27f1608d237b165ecb64f29e5bf48440a6faf00e
justicemerrick <justicemer@gmail.com>
2022-07-05 13:01:20 -0700

    Printing lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet -> prints the alphabets in lowercase
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
 	int j;
-
 	for (j = 'a'; j <= 'z'; j++)
 	{
-	_putchar(j);
+		_putchar(j);
 	}
-_putchar('\n');
-}	
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,3 @@
 A readme for 0x02. C - Functions, nested loops projects.
+Task 0. A program that prints _putchar, followed by a new line.
+Task 1. A function that prints the alphabet, in lowercase, followed by a new line.
