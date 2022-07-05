06b71735221224bce5532d881807a615bb9feebe
justicemerrick <justicemer@gmail.com>
2022-07-05 14:09:06 -0700

    print lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,15 @@
 /**
  * main - check the code
  *
+ * print_alphabet - prints the alphabet in lowercase
+ *
  * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
 	int j;
+	
 	for (j = 'a'; j <= 'z'; j++)
 	{
 		_putchar(j);
