c3dcb2b146fa686e090607b0227b7addfc492873
justicemerrick <justicemer@gmail.com>
2022-07-05 18:43:29 -0700

    Hot fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,8 +10,10 @@ int print_last_digit(int n)
 	int x;
 
 	if (n < 0)
-	n = -n;
-	x = n % 10;
-	_putchar(x + '0');
-	return (x);
+		x = -1 * (n % 10);
+	else
+		x = r % 10;
+
+	_putchar((x % 10) + '0');
+	return (x % 10);
 }
