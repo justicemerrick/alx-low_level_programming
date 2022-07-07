c836c13359de308526125ccb971cd9e65c87be0f
justicemerrick <justicemer@gmail.com>
2022-07-07 08:26:42 -0700

    _putchar.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-int mul(inta, int b);
+int mul(int a, int b);
 void more_numbers(void);
 void print_numbers(void);
 void Print_most_numbers(void);
