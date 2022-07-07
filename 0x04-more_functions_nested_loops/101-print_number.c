f686dd16147472ff15014ded2a97effcf646ac7e
justicemerrick <justicemer@gmail.com>
2022-07-07 12:34:07 -0700

    advanced tasks

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - a function that prints an integer
+ * @n: for the number
+ *
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int number = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+	if ((num / 10) > 0)
+	{
+		print_number(num / 10);
+	}
+	_putchar((num % 10) + '0');
+}
