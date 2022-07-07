5a2d03afdee679a676b28034c54c63998049cb06
justicemerrick <justicemer@gmail.com>
2022-07-07 13:40:07 -0700

    Hot fix

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -3,6 +3,7 @@
 
 /**
  * print_remaining_days - takes a date and prints remaining days
+ *
  * @month: month in number format
  * @day: day of month
  * @year: year
@@ -11,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
+	if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 	{
 		if (month > 2 && day >= 60)
 		{
