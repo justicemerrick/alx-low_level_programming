549c831827d501ebebac9c42ced2d197fd0b0037
justicemerrick <justicemer@gmail.com>
2022-07-11 15:54:36 -0700

    Task 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,19 +8,19 @@
 
 int _atoi(char *s)
 {
-	int sign = 1;
+	int nb = 1;
 	unsigned int num = 0;
 
 	do {
 		if (*s == '-')
-			sign *= -1;
+			nb *= -1;
 
 		else if (*s >= '0' && *s <= '9')
-			num + (num * 10) + (*s - '0');
+			num = (num * 10) + (*s - '0');
 
 		else if (num > 0)
 			break;
 	} while (*s++);
 
-	return (num * sign);
+	return (num * nb);
 }
