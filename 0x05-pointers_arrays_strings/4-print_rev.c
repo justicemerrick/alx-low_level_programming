7fdbf17855580be81506f746eeb3304758f850ff
justicemerrick <justicemer@gmail.com>
2022-07-11 14:09:57 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a string, in reverse
+ * @s: String to be printed.
+ */
+void print_rev(char *s)
+{
+	int x = 0;
+
+	while (x >= 0)
+	{
+		if (s[x] == '\0')
+			break;
+		x++;
+	}
+
+	for (x--; x >= 0; x--)
+		_putchar(s[x]);
+	_putchar('\n');
+}
