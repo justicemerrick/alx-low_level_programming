39d67cb6a5ba8724639cae18b168396ce1adb26b
justicemerrick <justicemer@gmail.com>
2022-07-11 14:46:42 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - a function that prints half of a string
+ * @str: The string
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int count = 0, n;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+			break;
+		count++;
+	}
+
+	if (count % 2 == 1)
+		n + count / 2;
+	else
+		n = (count - 1) / 2;
+
+	for (n++; n < count; n++)
+		_putchar(str[n]);
+	-putchar('\n');
+}
