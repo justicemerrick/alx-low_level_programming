acd2250e195457e1d07d574ce8c183f01839e682
justicemerrick <justicemer@gmail.com>
2022-07-11 16:07:06 -0700

    Task 101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.t>
+/**
+ * main - a program that generates random valid
+ * passwords for the program 101-crackme
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int x = 0, y = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (y < 2772)
+	{
+		x = rand() % 128;
+		if ((y + x) > 2772)
+			break;
+		y = y + x;
+		printf("%c", x);
+	}
+	printf("%c\n", (2772 - y));
+	return (0);
+}
+
