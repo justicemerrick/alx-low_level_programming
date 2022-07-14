c2ced0ec7d24b821de365374fee3a50adcb506f9
justicemerrick <justicemer@gmail.com>
2022-07-14 13:12:35 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,7 +11,16 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int x, y;
 
+	for (x = 0; dest[x] != '\0'; x++)
+	{
+		continue;
+	}
+	for (y = 0; src[j] != '\0' && j < n; j++)
+	{
+		dest[i + j] = src[j];
+	}
+	dest[i + j] = '\0';
 	return (dest);
 }
