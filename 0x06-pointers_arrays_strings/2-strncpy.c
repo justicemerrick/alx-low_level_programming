bcb08bb92bf6870f21ccccc0d09486adb7f2b3f9
justicemerrick <justicemer@gmail.com>
2022-07-14 12:45:10 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,7 +12,12 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	int x;
+
+	for (x = 0; x < n && src[x] != '\0'; x++)
+		dest[x] = src[x];
+	for ( ; x < n; x++)
+		dest[x] = '\0';
 
 	return (dest);
 }
