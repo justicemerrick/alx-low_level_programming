0d89c406899d94194a9a42caf51edaa04f358abc
justicemerrick <justicemer@gmail.com>
2022-07-15 12:06:16 -0700

    Task 102

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,10 +14,11 @@ int main(void)
 	a[2] = 1024;
 	p = &n;
 	/*
-	 * code introduced below
+	 * write your line of code here...
+	 * Remember:
 	 */
-	p[5] = 98;
-	/* ...so this prints 98\n */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
