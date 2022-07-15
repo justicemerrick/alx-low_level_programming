ab7ed216b3de2e15e02fb2027f3113c320e34c16
justicemerrick <justicemer@gmail.com>
2022-07-14 19:19:15 -0700

    Task 100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @c: input int
+ *
+ * Return: Void.
+ */
+void print_number(int c)
+{
+	unsigned int n;
+
+	n = c;
+
+	if (c < 0)
+	{
+		_putchar('-');
+		n = c;
+	}
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n1 % 10 + '0');
+}
