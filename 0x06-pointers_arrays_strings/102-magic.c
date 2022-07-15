f902810add5092ab011eae19010e58cb8baac8c7
justicemerrick <justicemer@gmail.com>
2022-07-15 12:10:16 -0700

    Hot fix

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,12 +13,11 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 */
+
 	*(p + 5) = 98;
+
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
+
 	return (0);
 }
