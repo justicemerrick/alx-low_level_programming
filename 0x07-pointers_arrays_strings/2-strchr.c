ddb697e1503677a8742d664059d88bb5d93063a7
justicemerrick <justicemer@gmail.com>
2022-07-18 13:36:44 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: String
+ * @c: Character.
+ * Return: The string from character found
+ */
+char *_strchr(char *s, char c)
+{
+	int x = 0, y;
+
+	while (s[x])
+	{
+		x++;
+	}
+
+	for (y = 0; y <= x; y++)
+	{
+		if (c == s[y])
+		{
+			s += y;
+			return (s);
+		}
+	}
+
+	return ('\0');
+}
