957b51f4b99c8dd8956614574c3c4613d6636809
justicemerrick <justicemer@gmail.com>
2022-07-20 01:58:40 -0700

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - a function that the length
+ * @s: String
+ *
+ * Return: 1
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0)
+		return (0);
+	return (1 + _strlen_recursions(s + 1));
+}
