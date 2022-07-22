92981d5580a4e8add67e9a4abae7df0a978d1355
justicemerrick <justicemer@gmail.com>
2022-07-22 14:38:55 -0700

    strlen.c

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns
+ * the length of a string
+ *
+ * @s: The string
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+		length++;
+
+	return (length); /*Return the length of the string*/
+}
