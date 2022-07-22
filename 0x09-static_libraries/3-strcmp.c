f3a7f61e9e99dc5a71fb9b953b9c0434436c8e81
justicemerrick <justicemer@gmail.com>
2022-07-22 15:14:34 -0700

    3-strcmp.c

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: 0 when s1 is equal to s2, and an int value if not
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
