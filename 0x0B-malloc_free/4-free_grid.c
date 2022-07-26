fff99f37d93bc5f0c7bd9e676ed7f5ee91cb2e76
justicemerrick <justicemer@gmail.com>
2022-07-26 13:39:29 -0700

    Task 3

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * free_grid - frees a 2 dimensional grid.
+ * free_grid - frees a two dimensional grid.
  * @grid: multidimensional array of integers.
  * @height: height of the grid.
  *
