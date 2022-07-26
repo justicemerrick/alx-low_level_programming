d8e5756618889789c3095a301becd67c4fb86001
justicemerrick <justicemer@gmail.com>
2022-07-26 02:59:41 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	s = (char *)malloc(size * sizeof(char));
+	b = (char *)malloc(size * sizeof(char));
 	if (b == NULL)
 		return (NULL);
 	for (a = 0; a < size; a++)
