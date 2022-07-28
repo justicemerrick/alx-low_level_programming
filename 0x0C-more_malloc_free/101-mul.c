4eb94e6c31922d12fb4f38a7d894fb464e83d0af
justicemerrick <justicemer@gmail.com>
2022-07-28 16:30:29 -0700

    Task 101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,10 +1,8 @@
 #include "main.h"
-#include <stdlib.h>
-#include <stdio.h>
 #include <ctype.h>
 
 /**
- * _is_zero - determines if any number is zero
+ * _is_zero - Function that determines if any number is zero
  * @argv: argument vector.
  *
  * Return: no return.
