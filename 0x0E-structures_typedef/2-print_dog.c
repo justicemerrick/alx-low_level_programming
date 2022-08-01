8662724601c25afe39a52da5b6ede4723e68f16a
justicemerrick <justicemer@gmail.com>
2022-08-01 15:29:46 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -20,11 +20,12 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else
 			printf("Name: %s\n", d->name);
+
 		printf("Age: %f\n", d->age);
 
 		if (d->owner == NULL)
-			printf("owner: (nil)\n");
+			printf("Owner: (nil)\n");
 		else
-			printf("owner: %s\n", d->owner);
+			printf("Owner: %s\n", d->owner);
 	}
 }
