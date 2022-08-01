f6d4870b085abf9b62e730caffd3d267d01879aa
justicemerrick <justicemer@gmail.com>
2022-08-01 14:44:31 -0700

    dog.h

---
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef STRTYP
+#define STRTYP
+
+/**
+ * struct dog - structure of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif /*STRTYP*/
