bad2d602f599d8d7e9048556e91b16c14409598f
justicemerrick <justicemer@gmail.com>
2022-08-11 09:19:46 -0700

    lists.h

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,14 +8,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	unsigned int count;
+	size_t nelem;
 
-	if (h == NULL)
-		return (0);
-	for (count = 0; h != NULL; count++)
+	nelem = 0;
+	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		nelem++;
 	}
-	return (count);
+	return (nelem);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -8,7 +8,7 @@
 /**
  * struct list_s - singly linked list
  * @str: str: string
- * @n: length of the string
+ * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
