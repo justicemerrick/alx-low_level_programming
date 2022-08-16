0ad20199ca2de5da332582b1c265414ef5702cbb
justicemerrick <justicemer@gmail.com>
2022-08-16 00:07:40 -0700

    Hot fix

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef _LISTS_
 #define _LISTS_
 
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly listed list
  * @n: integer
@@ -13,7 +16,7 @@ typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t print_listint(const listint_t *h);
